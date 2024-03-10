1d680d4af05ed871002feb7df7f472a613c04e22
Matheus Oliveira <matheus.oliveira@weconnectllc.com>
2024-03-09 21:05:29 -0700

    feat: 🎸 Initial project

---
+++ env.mjs
@@ -0,0 +1,16 @@
+
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    OPENAI_API_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+})
